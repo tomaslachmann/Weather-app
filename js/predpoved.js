de36f70ad3b161fe6f7c2070368981252fee369b
tomaslachmann <tomaslachmann@gmail.com>
2020-10-08 18:14:41 +0200

    pridani grafu

--- js/predpoved.js
+++ js/predpoved.js
@@ -56,5 +56,94 @@ for (let i = 0; i < dataWeather.length; i++){
     temp_min.appendChild(tr_min);
     temp_max.appendChild(tr_max);
 }
+const chartDataMax = dataWeather.map(function(e){
+  return Math.round(e.temp);
+});
+
+const chartDataMin = dataWeather.map(function(e){
+return Math.round(e.feels_like);
+});
+
+const chartNumberMax = (e) => {
+  return Math.max.apply(null, e);
+}
+
+
+const ctx = document.getElementById('chartMin').getContext('2d');
+const ctx1 = document.getElementById("chartMax").getContext("2d");
+
+
+const options = {legend: false,
+  tooltips: {enabled: false,},
+  cornerRadius: 20,
+    title: {
+      display: true,
+      text: ''
+    },
+  maintainAspectRatio: false,
+  scales: {
+      xAxes: [{
+        gridLines: {
+            display: false,
+            tickMarkLength: 0,
+            drawTicks: false,
+         },
+         ticks: {
+          max: chartNumberMax(chartDataMax),
+          min: 0,
+          display: false,
+       },
+         beginAtZero: true   
+      }],
+      yAxes: [{
+        gridLines: {
+            display: true,
+            offsetGridLines: false,
+            drawTicks: false,
+            borderDash: [5],
+            color:"#D1E4F8"
+            
+         },
+       ticks: {
+              display: false,
+              beginAtZero: true,
+            
+       }
+     }]
+   }}
+const maxChart = new Chart(ctx1, {
+    type: 'horizontalBar',
+    data: {
+      labels: chartDataMax,
+      datasets: [{
+          barPercentage: 0.5,
+          barThickness: 10,
+        label: '',
+        data: chartDataMax,
+        borderWidth: 0,
+        backgroundColor: "#FA4400",
+        radius: 5,
+      }]
+    },
+    responsive: false,
+    options:  options
+  });
+  
+  const minChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: chartDataMin,
+      datasets: [{
+          barPercentage: 0.5,
+          barThickness: 10,
+        label: '',
+        data: chartDataMin,
+        borderWidth: 0,
+        backgroundColor: "#D1E4F8",
+      }]
+    },
+    responsive: false,
+    options:options
+  });
 
 }
